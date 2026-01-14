a506ea068d3c4342209bffed6cae93682b41c9ec
g-devlt <gabriel.devasselot.de.regne@gmail.com>
2026-01-14 09:38:51 +0000

    Fixed factorial.py

--- debugging/factorial.py
+++ debugging/factorial.py
@@ -2,10 +2,14 @@
 import sys
 
 def factorial(n):
+    if not isinstance(n, int) or n < 0:
+        raise ValueError("n must be a non-negative integer")
+
     result = 1
     while n > 1:
         result *= n
         n -= 1
+
     return result
 
 f = factorial(int(sys.argv[1]))
